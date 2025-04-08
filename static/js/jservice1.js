f184443b5a80d0a1cf0a4ab73397e62ffcaa44c0
samruddhijp11 <126690392+samruddhijp11@users.noreply.github.com>
2025-04-08 20:51:41 +0530

    Create jservice1.js

---
+++ static/js/jservice1.js
@@ -0,0 +1,27 @@
+// app.js
+document.getElementById('diabetesForm').addEventListener('submit', async function(event) {
+    event.preventDefault();
+    
+    const data = {
+        gender: document.getElementById('gender').value,
+        age: document.getElementById('age').value,
+        fbs: document.getElementById('fbs').value,
+        ppbs: document.getElementById('ppbs').value,
+        hba1c: document.getElementById('hba1c').value,
+        bmi: document.getElementById('bmi').value,
+        weight: document.getElementById('weight').value,
+        bps: document.getElementById('bps').value,
+        bpd: document.getElementById('bpd').value
+    };
+
+    const response = await fetch('http://127.0.0.1:5000/predict1', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data)
+    });
+
+    const result1 = await response.json();
+    document.getElementById('result1').innerText = result1.prediction;
+});
