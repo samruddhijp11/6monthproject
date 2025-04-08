281986a07258f4efd33cfecb8b52a6160e47c1ed
samruddhijp11 <126690392+samruddhijp11@users.noreply.github.com>
2025-04-09 04:02:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,12 @@
 from flask import Flask
 from app import app as risk_app
 from app1 import app as diabetes_app
+import os
 
 app = Flask(__name__)
-
-# Register both apps as blueprints
 app.register_blueprint(risk_app, url_prefix='/risk')
 app.register_blueprint(diabetes_app, url_prefix='/diabetes')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
